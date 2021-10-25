4695223d4d881063d419643a287070fffffdffd2
Ilya Stasialovich <i.stasialovich.vironit@vironit.ru>
2021-10-25 13:10:46 +0300

    create design

---
+++ app/src/main/java/com/example/currencyconverter/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.example.currencyconverter.di
+
+object AppModule {
+}
---
+++ app/src/main/java/com/example/currencyconverter/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.currencyconverter.other
+
+object Constants {
+    const val API_KEY = "d13630f342efee317158eb3a91922037"
+}
